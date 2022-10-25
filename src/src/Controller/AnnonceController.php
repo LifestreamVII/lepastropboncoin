6cf8a3f7ad211e5ef19547cfcd61f21f1b807360
MihailSofroniev <98062952+MihailSofroniev@users.noreply.github.com>
2022-10-25 16:51:18 +0200

    user factory

--- src/src/Controller/AnnonceController.php
+++ src/src/Controller/AnnonceController.php
@@ -6,17 +6,19 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+#[Route('/annonce')]
 class AnnonceController extends AbstractController
 {
-    #[Route('/annonce', name: 'app_annonce')]
+    #[Route('/', name: 'app_annonce')]
     public function index(): Response
     {
         return $this->render('annonce/index.html.twig', [
             'controller_name' => 'AnnonceController',
+            'FIRSTNAM' => 'User',
         ]);
     }
 
-    #[Route('/annonce/add/{wildcard}', name: 'add_annonce')]
+    #[Route('/add', name: 'add_annonce')]
     public function add_annonce($wildcard, EntityManagerInterface $em)
     {
         $maNews = new Annonce();

2e744672311d8dc41713287d027b81fd236fce39
MihailSofroniev <98062952+MihailSofroniev@users.noreply.github.com>
2022-10-26 10:40:20 +0200

    controller and display test

--- src/src/Controller/AnnonceController.php
+++ src/src/Controller/AnnonceController.php
@@ -5,16 +5,19 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Repository\AnnonceRepository;
 
 #[Route('/annonce')]
 class AnnonceController extends AbstractController
 {
     #[Route('/', name: 'app_annonce')]
-    public function index(): Response
+    public function index(AnnonceRepository $repository): Response
     {
+        $annonces = $repository->findAll();
         return $this->render('annonce/index.html.twig', [
             'controller_name' => 'AnnonceController',
             'FIRSTNAM' => 'User',
+            'annonces' => $annonces,
         ]);
     }
 

9e73b7f9790f39e59bcc44efec63faefdeefe52a
MihailSofroniev <98062952+MihailSofroniev@users.noreply.github.com>
2022-10-30 21:23:41 +0100

    factories forms annonceController and vote entity

--- src/src/Form/AnnonceType.php
+++ src/src/Form/AnnonceType.php
@@ -3,9 +3,13 @@
 namespace App\Form;
 
 use App\Entity\Annonce;
+use App\Entity\Tag;
+use App\Repository\TagRepository;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\Count;
 
 class AnnonceType extends AbstractType
 {
@@ -16,8 +20,18 @@ class AnnonceType extends AbstractType
             ->add('prix')
             ->add('description')
             ->add('date')
-            ->add('auteur')
-            ->add('tags')
+            ->add('tags', EntityType::class, [
+                'class' => Tag::class,
+                'query_builder' => function (TagRepository $tagRepository) {
+                    return $tagRepository->createQueryBuilder('t')
+                        ->orderBy('t.libelle', 'ASC')
+                        ->setMaxResults(10);
+                },
+                'choice_label' => 'libelle',
+                'multiple' => true,
+                'mapped' => false,
+                'constraints' => new Count(null, 1, 3),
+            ])
         ;
     }
 
